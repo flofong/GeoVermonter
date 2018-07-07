bffcff2be06e44e5a581e8291616ce76497c65b2
Joshua Burke <burke.joshua.james@gmail.com>
2018-07-07 17:15:57 -0400

    Configuration and bootstrapped files from Cypress

---
+++ cypress/integration/geo_vermonter_tests/ui_spec.js
@@ -0,0 +1,12 @@
+describe('Smoke Test', function () {
+  it('Just checks if tests run', function () {
+    expect(true).to.equal(true);
+  });
+});
+
+describe('Page Navigation', function () {
+  it('Should have a map container', function () {
+    cy.visit('http://localhost:5000/');
+    cy.contains('map');
+  });
+});
