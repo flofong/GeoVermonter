9bc2f3b095fc52e42b8ce210366fc3a3499c4fd0
Joshua Burke <burke.joshua.james@gmail.com>
2018-07-07 20:55:08 -0400

    Use Cypress 'baseUrl' instead of prefixing 'localhost:5000' to URLs

--- cypress/integration/geo_vermonter_tests/ui_spec.js
+++ cypress/integration/geo_vermonter_tests/ui_spec.js
@@ -6,7 +6,7 @@ describe('Smoke Test', function () {
 
 describe('Page Navigation', function () {
   it('Should have a map container', function () {
-    cy.visit('http://localhost:5000/');
+    cy.visit('/');
     cy.get('#map');
   });
 });
