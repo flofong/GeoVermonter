dd9e84945680909ef3731d90f09406043d41aea6
Joshua Burke <burke.joshua.james@gmail.com>
2018-07-08 17:44:42 -0400

    Add Vermont border to the Leaflet map for displaying state region

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-var mymap = L.map('map').setView([44.4759, -73.2121], 7.5);
-L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-  maxZoom: 18,
-  minZoom: 1,
-	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-}).addTo(mymap);
+let mymap = L.map('map').setView([43.5588, -72.5778], 7);
+
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+  {
+    maxZoom: 18,
+    minZoom: 1,
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+  }).addTo(mymap);
+
+L.geoJSON(data).addTo(mymap);
